8af21029690e1fea2cd895d8af8a0acef62586f8
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2022-06-06 07:02:20 +0100

    experimental javascript file for horoscope generator

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-//just a little sketch for a random message generator
 /*
-That's a shame that you are feelign..., but well done for... I hope that your... will be a success
 
-astrology
-
-you should be courageous, such and such is a sign that 
-
-]PLANET will stop spinning which will leave you feeling discombobulated as the 
-
-retrograde mercury
-
-BELOW
-
-mercury is (no longer in/in) retrograde, so (you'll need to wait three months until) you can start blaming all your problems on retrograde mercury (again)
-
-On [day] the moon in [STARSIGN] connects with [PLANET] in [different STARSIGN], which could stir up [EMOTION]
-
-a problem at (work / home / a social group/ church / temple /) could mean it's time to stop (holding back, making excuses, fighting yourself, worrying, putting yourself down)
-
-is a good day to (ask for a raise / ask an old friend out for dinner / make your move on that mysterious individual you've been flirting with)
-
-When the Moon enters Leo
-
-The Moon in cancer moving into Uranus is a sign that discomfort could be in store BARE LOLZ!!!
-this could end lots of different ways will cause (discomfort / the discovery of a pleasant new sensation / you to stop fighting your true feelings)
-
-Staring directly into the sun on DAY will bring good luck when you (ask for a raise / reveal your feelings to an old friend / declare war on a neighbouring country and force your ideology on its people / ask out the barista you've been flirting with / watch your team playing this week)
+This is just a trial bit of code to get a sense of what the project will involve. It generates horoscopes with four sentences.
 
 */
+
+//sentence 1
 
 function retrogradeDecision () {
     let opener = 'Mercury is ';
@@ -52,8 +29,7 @@ function retrogradeDecision () {
 
 console.log(retrogradeDecision());
 
-
-//On [day] the moon in [STARSIGN] connects with [PLANET] in [different STARSIGN], which could stir up [EMOTION]
+//databank used by sentences 2 to 4
 
 const data = {
     day: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],    
@@ -65,6 +41,10 @@ const data = {
     luckyEvent: ['ask for a raise', 'reveal your feelings to an old friend', 'declare war on a neighbouring country and force your ideology on its people', "ask out the barista you've been flirting with", 'watch your team playing this week']
 }
 
+//sentence 2
+//format On [day] the moon in [STARSIGN] connects with [PLANET] in [different STARSIGN], which could stir up [EMOTION]
+
+
 function stirredUp () {
     let dayNumber = Math.floor(Math.random() * 7);
     let starsignNumber = Math.floor(Math.random() * 12);
@@ -74,8 +54,10 @@ function stirredUp () {
     return `On ${data['day'][dayNumber]} the moon in ${data['starsign'][starsignNumber]} connects with ${data['planet'][planetNumber]} in ${data['starsign'][starsignNumber2]}, which could stir up ${data['emotion'][emotionNumber]}.`;
 }
 
-//console.log(data['day'][1]);
+
 console.log(stirredUp());
+
+//sentence 3
 
 function habitStop() {
     let locationNumber = Math.floor(Math.random() * 5);
@@ -84,6 +66,8 @@ function habitStop() {
 }
 
 console.log(habitStop());
+
+//sentence 4
 
 function luckyTip() {
     let dayNumber2 = Math.floor(Math.random() * 7);
